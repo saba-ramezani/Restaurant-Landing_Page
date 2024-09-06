a084262b451a45509da2a9376768de89a4db9afd
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-06 17:38:18 +0330

    added title prop to subheading component

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {images} from '../../constants';
 
-const SubHeading = () => (
+const SubHeading = ({title}) => (
   <div style={{marginBottom: '1rem'}}>
-    <p className='p__cormorant'>TITLE</p>
+    <p className='p__cormorant'>{title}</p>
     <img src={images.spoon} alt="spoon" className='spoon__img' />
   </div>
 );
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,7 +6,8 @@ import { SubHeading } from '../../components';
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className='app__wrapper_info'>
-      <SubHeading/>
+      <SubHeading title="Chase the New Flavour"/>
+      <h1 className='app__header-h1'>The Key To Fine Dining</h1>
     </div>
     <div className='app__wrapper_img'>
 
