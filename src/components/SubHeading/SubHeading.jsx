588934802053cb0cb5ce4411802b5f70ac01eaf6
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-06 17:36:00 +0330

    added initial design of subheading component

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {images} from '../../constants';
 
 const SubHeading = () => (
-  <div>
-    SubHeading
+  <div style={{marginBottom: '1rem'}}>
+    <p className='p__cormorant'>TITLE</p>
+    <img src={images.spoon} alt="spoon" className='spoon__img' />
   </div>
 );
 
