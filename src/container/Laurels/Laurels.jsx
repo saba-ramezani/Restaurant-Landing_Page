0eccd34a78ea637e2ed9195440a8ea5cb660f9e6
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 01:19:46 +0330

    added AwardCard component in awards section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -3,6 +3,17 @@ import { SubHeading } from '../../components';
 import { images, data } from '../../constants';
 import './Laurels.css';
 
+
+const AwardCard = ({award: {imgUrl, title, subtitle}}) => (
+  <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt="" />
+    <div className='app__laurels_awards-card_content'>
+      <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      <p className="p__opensans">{subtitle}</p>
+    </div>
+  </div>
+)
+
 const Laurels = () => (
   <div className='app__bg app__wrapper section__padding' id='awards'>
     <div className='app__wrapper_info'>
