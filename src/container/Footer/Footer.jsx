9d83bba5eb227d6c68475b5b3dd105bb7bb0762b
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 02:10:22 +0330

    added overlay and newsletter to footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
