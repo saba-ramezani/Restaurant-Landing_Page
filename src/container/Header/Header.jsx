94034a57883eba688904a1f7b9f0873b6a278b3d
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-06 17:31:01 +0330

    added initial design of header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import './Header.css';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading/>
+    </div>
+    <div className='app__wrapper_img'>
+
+    </div>
   </div>
 );
 
