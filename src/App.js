3b6bd2c859179a72e720eb2aa683523f045a87b2
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-06 15:12:48 +0330

    added initial desgines of all components and containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  )
-}
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
