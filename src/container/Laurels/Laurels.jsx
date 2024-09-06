cc2e4aa9c68bd5a88d75058dd4f18f2fd04a38de
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-07 01:15:50 +0330

    added initial design of laurels section with no styling

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Awards & recognition"/>
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+      <div className='app__laurels_awards'>
+        {data.awards.map((award, index) => <AwardCard award={award} key={award.title + index}/>)}
+      </div>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.laurels} alt="laurels img" />
+    </div>
   </div>
 );
 
